456cbef9e598b29ef09a45e6ccb3f64a28090daf
Brandi <brandi.w.crawford@nyu.edu>
2013-12-05 13:33:22 -0500

    Create int3.rb

---
+++ int3.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/ruby -I/msf3/lib -Ilib
+
+require 'bisc'
+
+#
+# Create our BISC object, which will handle scanning for instruction
+# sequences and managing scratch data space
+#
+bisc = BISC::Assembler.new(ARGV)
+
+#
+# Build and test a simple return-oriented program that
+# executes an "INT3"
+#
+Main = [
+	"INT3"
+]
+
+print bisc.assemble(Main)
